33d2c1f24a2bf0d4a23beaf13d0d5a0a3fdd1f36
Justin <justinv403@gmail.com>
2023-11-08 15:21:44 -0600

    Work on Player (Again)

--- PPG3/Assets/Scripts/FirstPersonAnimator.cs
+++ PPG3/Assets/Scripts/FirstPersonAnimator.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class FirstPersonAnimator : MonoBehaviour
 {
-    // variable to hold animator from child object
-    Animator[] animators;
+    // animator variables
+    Animator visibleAnimator;
+    Animator shadowAnimator;
 
     // get FirstPersonController script
     private FirstPersonController fpsController;
@@ -18,13 +19,13 @@ public class FirstPersonAnimator : MonoBehaviour
         // get animator from child
         remyVisible = GameObject.Find("RemyVisible");
         remyShadow = GameObject.Find("RemyShadow");
-        animators[0] = remyVisible.GetComponent<Animator>();
-        animators[1] = remyShadow.GetComponent<Animator>();
+
+        visibleAnimator = remyVisible.GetComponent<Animator>();
+        shadowAnimator = remyShadow.GetComponent<Animator>();
 
         // get FPS controller
         fpsController = gameObject.GetComponentInChildren<FirstPersonController>();
 
-        Debug.Log("remyVisible = " + remyVisible);
     }
 
     // Update is called once per frame
@@ -33,30 +34,26 @@ public class FirstPersonAnimator : MonoBehaviour
         // set the walk state of the player
         float h = Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical"));
 
-        /*
         if(h > 0 && !fpsController.isCrouched)
         {
-            animators[0].SetFloat("WalkState", 1.0f);
-            animators[1].SetFloat("WalkState", 1.0f);
+            visibleAnimator.SetFloat("WalkState", 1.0f);
+            shadowAnimator.SetFloat("WalkState", 1.0f);
 
         } else if (h > 0 && fpsController.isCrouched)
         {
-            animators[0].SetFloat("WalkState", 0.66f);
-            animators[1].SetFloat("WalkState", 0.66f);
+            visibleAnimator.SetFloat("WalkState", 0.66f);
+            shadowAnimator.SetFloat("WalkState", 0.66f);
 
         } else if (h == 0 && fpsController.isCrouched)
         {
-            animators[0].SetFloat("WalkState", 0.33f);
-            animators[1].SetFloat("WalkState", 0.33f);
+            visibleAnimator.SetFloat("WalkState", 0.33f);
+            shadowAnimator.SetFloat("WalkState", 0.33f);
 
         } else
         {
-            animators[0].SetFloat("WalkState", 0.0f);
-            animators[1].SetFloat("WalkState", 0.0f);
+            visibleAnimator.SetFloat("WalkState", 0.0f);
+            shadowAnimator.SetFloat("WalkState", 0.0f);
         }
-        */
-        
-        
         
     }
 }

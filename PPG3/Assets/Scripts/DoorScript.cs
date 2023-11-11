a6d2677f64b1c0a041319342ed7c7b05268be31a
Justin <justinv403@gmail.com>
2023-11-10 18:23:51 -0600

    Minor Improvements to Doors

--- PPG3/Assets/Scripts/DoorScript.cs
+++ PPG3/Assets/Scripts/DoorScript.cs
@@ -7,21 +7,29 @@ public class DoorScript : MonoBehaviour
     private float startAngle;
     private float minAngle = 0f;
     private float maxAngle = 90f;
+    private bool isDragging = false;
 
     public void StartInteraction(Vector3 dragPosition)
     {
+        isDragging = true;
         startDragPosition = dragPosition;
         startAngle = hinge.localEulerAngles.y;
     }
 
+    public void EndInteraction()
+    {
+        isDragging = false;
+    }
+
     public void Interact(Vector3 dragPosition)
     {
-        Vector3 hingeToStartDragPosition = startDragPosition - hinge.position;
-        Vector3 hingeToCurrentDragPosition = dragPosition - hinge.position;
-
-        float angle = Vector3.SignedAngle(hingeToStartDragPosition, hingeToCurrentDragPosition, hinge.up);
-
-        float newAngle = Mathf.Clamp(startAngle + angle, minAngle, maxAngle);
-        hinge.localRotation = Quaternion.Euler(0, newAngle, 0);
+        if (isDragging)
+        {
+            Vector3 hingeToStartDragPosition = startDragPosition - hinge.position;
+            Vector3 hingeToCurrentDragPosition = dragPosition - hinge.position;
+            float angle = Vector3.SignedAngle(hingeToStartDragPosition, hingeToCurrentDragPosition, hinge.up);
+            float newAngle = Mathf.Clamp(startAngle + angle, minAngle, maxAngle);
+            hinge.localRotation = Quaternion.Euler(0, newAngle, 0);
+        }
     }
 }
--- PPG3/Assets/Scripts/PlayerRaycastScript.cs
+++ PPG3/Assets/Scripts/PlayerRaycastScript.cs
@@ -5,7 +5,6 @@ public class PlayerRaycastScript : MonoBehaviour
     public float interactionDistance = 5f;
     private Camera mainCamera;
     private DoorScript selectedDoor;
-    private float distanceToSelectedDoor = 0f; // track distance to door
 
     void Start()
     {
@@ -16,36 +15,34 @@ public class PlayerRaycastScript : MonoBehaviour
     {
         // initialize raycast
         RaycastHit hit;
-
         if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, interactionDistance))
         {
             DoorScript doorScript = hit.transform.GetComponent<DoorScript>();
             if (doorScript != null)
             {
-                if(distanceToSelectedDoor == interactionDistance){
-                    distanceToSelectedDoor = hit.distance; // Update the distance
-                }
-                
-                
                 if (Input.GetMouseButtonDown(0))
                 {
-                    selectedDoor = doorScript;
-                    selectedDoor.StartInteraction(hit.point);
+                    if (selectedDoor == null)
+                    {
+                        selectedDoor = doorScript;
+                        selectedDoor.StartInteraction(hit.point);
+                    }
                 }
-
             }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            selectedDoor = null;
-            distanceToSelectedDoor = interactionDistance;
+            if (selectedDoor != null)
+            {
+                selectedDoor.EndInteraction();
+                selectedDoor = null;
+            }
         }
 
         if (selectedDoor != null)
         {
             selectedDoor.Interact(hit.point);
         }
-
     }
 }
